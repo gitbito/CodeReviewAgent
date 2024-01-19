b03c37120f8f15d302fd2903f194074043ce9e89
bitoandy <125080245+bitoandy@users.noreply.github.com>
2024-01-18 16:21:31 -0800

    Code Review Action initial checkin
    YAML, Docker and action entry point for GitHub Actions initial checkin

---
+++ cra-action/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+# Arguments to run the Docker image
+echo $INPUT_PR
+echo $INPUT_COMMAND
+echo $INPUT_OPTIONS
+
+SUPPORTED_COMMANDS=("/review")
+
+INPUT_COMMAND=$(echo "$INPUT_COMMAND" | tr -d '[:space:]')
+for command in "${SUPPORTED_COMMANDS[@]}"; do
+  if [ "$command" = "$INPUT_COMMAND" ]; then
+    valid_command=true
+    break
+  fi
+done
+
+
+# Run the Docker container from the specified image
+if [ "$valid_command" = true ]; then
+  exec docker run bitoai/cra:latest --mode=cli --pr_url $INPUT_PR $INPUT_COMMAND $INPUT_OPTIONS
+else
+  echo "$INPUT_COMMAND is not supported"
+  exit 0  # Exit the script with a non-zero status code
+fi
