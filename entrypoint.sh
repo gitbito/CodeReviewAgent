94384bd7092e0b3310af2fb25415fc794bdfd6a9
rishabhbohra-bito <157113574+rishabhbohra-bito@users.noreply.github.com>
2024-05-08 14:13:12 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,13 @@ echo $INPUT_OPTIONS
 
 SUPPORTED_COMMANDS=("/review" "review")
 
-INPUT_COMMAND=$(echo "$INPUT_COMMAND" | tr -d '[:space:]')
+#INPUT_COMMAND=$(echo "$INPUT_COMMAND" | tr -d '[:space:]')
+INPUT_COMMAND=$(echo "$INPUT_COMMAND" | xargs)
+
+
+# Check if the command starts with any of the supported commands
 for command in "${SUPPORTED_COMMANDS[@]}"; do
-  if [ "$command" = "$INPUT_COMMAND" ]; then
+  if [[ "$INPUT_COMMAND" =~ ^$command ]]; then
     valid_command=true
     break
   fi
@@ -20,7 +24,7 @@ done
 # Run the Docker container from the specified image
 if [ "$valid_command" = true ]; then
   docker pull bitoai/cra:latest >&2
-  exec docker run bitoai/cra:latest --mode=cli --pr_url $INPUT_PR $INPUT_COMMAND $INPUT_OPTIONS
+  exec docker run bitoai/cra:latest --mode=cli --pr_url $INPUT_PR --command $INPUT_COMMAND $INPUT_OPTIONS
 else
   echo "$INPUT_COMMAND is not supported"
   exit 0  # Exit the script with a non-zero status code
